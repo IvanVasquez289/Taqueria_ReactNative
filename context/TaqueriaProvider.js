92c6544aafcce6f760eb2bc9f00aa9cafc9e5ec4
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-08-14 21:21:43 -0600

    Setting up async storage

--- context/TaqueriaProvider.js
+++ context/TaqueriaProvider.js
@@ -1,9 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createContext, useState,useEffect } from "react";
 
 const TaqueriaContext = createContext()
 
 const TaqueriaProvider = ({children}) => {
     const [saludo,setSaludo] = useState('Hola desde estado')
+
+    const setData = async () => {
+        try {
+            await AsyncStorage.setItem('key','este es el key')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    useEffect(() => {
+     setData()   
+    }, [])
+    
     return (
         <TaqueriaContext.Provider
             value={{
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { View, Text, TouchableOpacity , ScrollView} from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
+
+  const handlePressStorage = async () => {
+    const mensaje = await AsyncStorage.getItem('key')
+    console.log(mensaje)
+  }
   return (
     <ScrollView>
       <View>
@@ -31,6 +37,22 @@ const HomeScreen = () => {
             Ir a StackScreen
           </Text>
         </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handlePressStorage}
+          style={{
+            backgroundColor:'blue',
+            padding:15,
+            marginTop:'20%',
+            width:'50%',
+            alignSelf:'center',
+            borderRadius:10
+          }}
+        >
+          <Text style={{textAlign:'center', color:'white', fontSize:20, fontWeight:'bold'}}>
+            Mostrar mensaje de storage
+          </Text>
+        </TouchableOpacity>
+
       </View>
     </ScrollView>
   )
